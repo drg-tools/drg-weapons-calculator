f1635945a8de74265e70980d4c7d6c6b89256452
The Phil <phg49389@bethel.edu>
2020-05-09 14:02:46 -0500

    Small fixup commit
    Added an ackno for GSG letting me use their images, and refactored new TCA thread to be a little cleaner.

--- src/guiPieces/InformationTabsText.java
+++ src/guiPieces/InformationTabsText.java
@@ -85,6 +85,12 @@ public class InformationTabsText {
 			{"", ""},
 			{"", ""},
 			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
 			// {"", ""},
 		};
 		
@@ -166,12 +172,19 @@ public class InformationTabsText {
 	
 	public static JPanel getAcknowledgementsText() {
 		String[][] acknowledgementsText = {
+			{"Ghost Ship Games", "Thank you for making the game Deep Rock Galactic and letting me use some images and artwork from the game in this program."},
 			{"Ian McDonagh", "Thank you for creating the open-source JAR 'image4j' that allows me to use .ico files natively."},
 			{"Gaming for the Recently Deceased", "Thank you for helping to promote this project and making a video about it. YouTube Channel: https://www.youtube.com/channel/UCL_8gMChYJD5ls7GaJtGmUw"},
 			{"Usteppin", "Thank you for collect some data and test weapon builds for me on Hazard 5. Twitch Channel: https://www.twitch.tv/usteppin"},
 			{"Elythnwaen", "Thank you for collecting data about elemental weaknesses, resistances, Burn/Freeze temperatures, and more! Also, thank you for letting me know about Subata's 50% Armor Breaking penalty and helping me figure out the correct Armor Breaking formula."},
 			{"Alpha and Beta testers", "Thank you Minomess, Royal, CynicalAtropos, and ARobotWithCancer for giving me feedback while this was still being developed and helping test out the builds."},
 			// I'm intentionally adding blank lines below here so that the content gets pushed to the top of the page
+			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
+			{"", ""},
 			{"", ""},
 			{"", ""},
 			{"", ""},
--- src/guiPieces/ThinkingCursorAnimation.java
+++ src/guiPieces/ThinkingCursorAnimation.java
@@ -1,7 +1,5 @@
 package guiPieces;
 
-import java.awt.Cursor;
-
 import javax.swing.JFrame;
 
 // Adapted from http://www.java2s.com/Tutorial/Java/0240__Swing/ChangeCursorinathreadforanimation.htm
@@ -10,13 +8,11 @@ public class ThinkingCursorAnimation implements Runnable {
 	private JFrame guiToChangeCursor;
 	private boolean animate;
 	private int msDelayBetweenFrames;
-	private Cursor[] animatedCursorFrames;
 	
 	public ThinkingCursorAnimation(JFrame gui) {
 		guiToChangeCursor = gui;
 		animate = false;
 		msDelayBetweenFrames = 100;
-		animatedCursorFrames = CustomCursors.thinkingCursor;
 	}
 	
 	public void toggleAnimation() {
@@ -27,11 +23,11 @@ public class ThinkingCursorAnimation implements Runnable {
 	public void run() {
 		int counter = 0;
 		while (animate) {
-			guiToChangeCursor.setCursor(animatedCursorFrames[counter]);
-			counter = (counter + 1) % animatedCursorFrames.length;
+			guiToChangeCursor.setCursor(CustomCursors.thinkingCursor[counter]);
+			counter = (counter + 1) % CustomCursors.thinkingCursor.length;
 			
 			try {
-				Thread.currentThread().sleep(msDelayBetweenFrames);
+				Thread.sleep(msDelayBetweenFrames);
 			} 
 			catch (Exception e) {
 			
