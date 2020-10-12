bc84740f3f5643bf889896f6d94bfc81ca7c161d
The Phil <phg49389@bethel.edu>
2020-10-12 15:12:54 -0500

    Implemented on-papter changes for Cryo Cannon

--- src/drillerWeapons/Flamethrower.java
+++ src/drillerWeapons/Flamethrower.java
@@ -10,7 +10,6 @@ import guiPieces.customButtons.ButtonIcons.modIcons;
 import guiPieces.customButtons.ButtonIcons.overclockIcons;
 import modelPieces.UtilityInformation;
 import modelPieces.DoTInformation;
-import modelPieces.DwarfInformation;
 import modelPieces.EnemyInformation;
 import modelPieces.Mod;
 import modelPieces.Overclock;
