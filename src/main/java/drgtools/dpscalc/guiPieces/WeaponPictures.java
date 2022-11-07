92532fb29d2cb7b3d237403c767c08f4b5509e73
The Phil <phg49389@bethel.edu>
2022-11-07 13:19:11 -0600

    Moved Images and Fonts to /resources
    For Eclipse, all I had to do was add it as a source folder. Not very intuitive, but happy I solved it without internet.

--- src/main/java/drgtools/dpscalc/guiPieces/WeaponPictures.java
+++ src/main/java/drgtools/dpscalc/guiPieces/WeaponPictures.java
@@ -5,29 +5,27 @@ import java.awt.image.BufferedImage;
 import drgtools.dpscalc.utilities.ResourceLoader;
 
 public class WeaponPictures {
-	private static String pathPrefix = "images";
-	
 	// Driller
-	public static BufferedImage flamethrower = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_FlameThrower.png");
-	public static BufferedImage cryoCannon = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_Cryospray.png");
-	public static BufferedImage subata = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_Pistol.png");
-	public static BufferedImage EPC = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_ChargeBlaster.png");
+	public static BufferedImage flamethrower = ResourceLoader.loadImage("images/weapons/GearGraphic_FlameThrower.png");
+	public static BufferedImage cryoCannon = ResourceLoader.loadImage("images/weapons/GearGraphic_Cryospray.png");
+	public static BufferedImage subata = ResourceLoader.loadImage("images/weapons/GearGraphic_Pistol.png");
+	public static BufferedImage EPC = ResourceLoader.loadImage("images/weapons/GearGraphic_ChargeBlaster.png");
 	
 	// Engineer
-	public static BufferedImage shotgun = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_CombatShotgun.png");
-	public static BufferedImage SMG = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_SMG.png");
-	public static BufferedImage grenadeLauncher = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_GrenadeLauncher.png");
-	public static BufferedImage breachCutter = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_LineCutter.png");
+	public static BufferedImage shotgun = ResourceLoader.loadImage("images/weapons/GearGraphic_CombatShotgun.png");
+	public static BufferedImage SMG = ResourceLoader.loadImage("images/weapons/GearGraphic_SMG.png");
+	public static BufferedImage grenadeLauncher = ResourceLoader.loadImage("images/weapons/GearGraphic_GrenadeLauncher.png");
+	public static BufferedImage breachCutter = ResourceLoader.loadImage("images/weapons/GearGraphic_LineCutter.png");
 	
 	// Gunner
-	public static BufferedImage minigun = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_GatlingGun.png");
-	public static BufferedImage autocannon = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_AutoCannon.png");
-	public static BufferedImage revolver = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_Revolver.png");
-	public static BufferedImage burstPistol = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_BurstPistol.png");
+	public static BufferedImage minigun = ResourceLoader.loadImage("images/weapons/GearGraphic_GatlingGun.png");
+	public static BufferedImage autocannon = ResourceLoader.loadImage("images/weapons/GearGraphic_AutoCannon.png");
+	public static BufferedImage revolver = ResourceLoader.loadImage("images/weapons/GearGraphic_Revolver.png");
+	public static BufferedImage burstPistol = ResourceLoader.loadImage("images/weapons/GearGraphic_BurstPistol.png");
 	
 	// Scout
-	public static BufferedImage assaultRifle = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_AssaultRifle.png");
-	public static BufferedImage classic = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_BoltActionRifle.png");
-	public static BufferedImage boomstick = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_SawedOffShotgun.png");
-	public static BufferedImage zhukovs = ResourceLoader.loadImage(pathPrefix + "/weapons/GearGraphic_DuelMachinePistols_DMP_MK_II.png");
+	public static BufferedImage assaultRifle = ResourceLoader.loadImage("images/weapons/GearGraphic_AssaultRifle.png");
+	public static BufferedImage classic = ResourceLoader.loadImage("images/weapons/GearGraphic_BoltActionRifle.png");
+	public static BufferedImage boomstick = ResourceLoader.loadImage("images/weapons/GearGraphic_SawedOffShotgun.png");
+	public static BufferedImage zhukovs = ResourceLoader.loadImage("images/weapons/GearGraphic_DuelMachinePistols_DMP_MK_II.png");
 }
